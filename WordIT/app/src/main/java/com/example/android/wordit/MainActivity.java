a77537c34597ade33123855ef8adb6aa7078f134
Omkar Tuppe <omkarvt13@icloud.com>
2018-01-15 19:18:42 +0530

    Update MainActivity.java

--- WordIT/app/src/main/java/com/example/android/wordit/MainActivity.java
+++ WordIT/app/src/main/java/com/example/android/wordit/MainActivity.java
@@ -1,106 +1,111 @@
 package com.example.android.wordit;
-
 /**
  * Created by omkartuppe on 01/01/18.
  */
-import android.util.Log;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.res.AssetManager;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Random;
 
-public class WordDictionary {
-    private HashMap<String,HashSet<String>> lettersToWorld;
-    public HashSet<String> result;
-    private Random random;
-    public WordDictionary(InputStream wordListStream) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(wordListStream));
-        lettersToWorld = new HashMap<String,HashSet<String>>();
-        random = new Random();
-        String line = null;
-        while((line = in.readLine()) != null) {
-            String word = line.trim();
-            String key = sortLetters(word);
-            if(word.length() == 4 || word.length() == 5){
-                if(!lettersToWorld.containsKey(key)) {
-                    lettersToWorld.put(key, new HashSet<String>());
-                    lettersToWorld.get(key).add(word);
-                }
-                else{
-                    lettersToWorld.get(key).add(word);
-                }
-            }
+public class MainActivity extends AppCompatActivity {
+    private int score;
+    private TextView text,timer,scoreText,resultview;
+    private EditText newWord;
+    Button btnch,btnrest;
+    private WordDictionary wordDictionary;
+    private CountDownTimer countDownTimer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AssetManager assetManager = getAssets();
+        try {
+            InputStream inputStream = assetManager.open("words.txt");
+            wordDictionary = new WordDictionary(inputStream);
+        } catch (IOException e) {
+            Toast toast = Toast.makeText(this, "Could not load dictionary", Toast.LENGTH_LONG);
+            toast.show();
         }
+        onStart(null);
     }
-    public boolean isWord(String word) {
-        String sortedOrder = sortLetters(word);
-        if(lettersToWorld.containsKey(sortedOrder)){
-            if(lettersToWorld.get(sortedOrder).contains(word)){
-                return true;
-            }
-        }
-        return false;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 
-    public String getNewLettersSequence(){
-        result = new HashSet<String>();
-        String word = new String();
-        ArrayList<String> sequences = new ArrayList(lettersToWorld.keySet());
-        int index = random.nextInt(sequences.size());
-        word = sequences.get(index);
-        Log.i("word",word);
-        int first,second;
-        first = random.nextInt(25) + 97;
-        second = random.nextInt(25) + 97;
-        index = random.nextInt(25) + 97;
-        char []temp = new char[5];
-        if(word.length() == 4){
-            word = word + Character.toString((char)first) + Character.toString((char)second) + Character.toString((char)index) ;
-            word = sortLetters(word);
-            getResultantWords(word.toCharArray(),temp, 0, 6, 0, 4);
-            getResultantWords(word.toCharArray(),temp, 0, 6, 0, 5);
-        }
-        else {
-            word = word + Character.toString((char) first) + Character.toString((char) second);
-            word = sortLetters(word);
-            getResultantWords(word.toCharArray(),temp, 0, 6, 0, 4);
-            getResultantWords(word.toCharArray(),temp, 0, 6, 0, 5);
-        }
-        Log.i("word",word);
-        return word;
-    }
-    public String sortLetters(String word){
-        char [] letters = word.toCharArray();
-        Arrays.sort(letters);
-        String alphabeticalOrder = new String(letters);
-        return alphabeticalOrder;
-    }
-    public void getResultantWords(char[] arr, char [] tempResultantWord, int start, int end, int index, int wordLength)
-    {
-        String resultantWord = new String();
-        if (index == wordLength)
-        {
-            resultantWord = "";
-            for (int j=0; j< wordLength ; j++)
-                resultantWord = resultantWord + Character.toString(tempResultantWord[j]);
-            Log.i("word",resultantWord);
-            resultantWord = sortLetters(resultantWord);
-            if(lettersToWorld.containsKey(resultantWord)){
-                result.addAll(lettersToWorld.get(resultantWord));
-            }
-            return;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
         }
 
-        for (int i=start; i<=end && end-i+1 >= wordLength - index; i++)
-        {
-            tempResultantWord[index] = arr[i];
-            getResultantWords(arr, tempResultantWord, i+1, end, index+1, wordLength);
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public boolean onStart(View view) {
+        timer = (TextView) findViewById(R.id.timer);
+        btnch = (Button) findViewById(R.id.button_check);
+        btnrest = (Button) findViewById(R.id.button_restart);
+        text = (TextView) findViewById(R.id.randomText);
+        newWord = (EditText) findViewById(R.id.newWord);
+        scoreText = (TextView) findViewById(R.id.score);
+        resultview = (TextView) findViewById(R.id.results);
+        score = 0;
+        String word = wordDictionary.getNewLettersSequence();
+        text.setText(word);
+        countDownTimer = new CountDownTimer(20000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timer.setText(""+millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                btnch.setVisibility(View.INVISIBLE);
+                resultview.append(TextUtils.join("\n",wordDictionary.result));
+            }
+        }.start();
+        return true;
+    }
+
+    public void onCheck(View view){
+        String word = newWord.getText().toString();
+        if(wordDictionary.isWord(word)){
+            score = Integer.parseInt(scoreText.getText().toString());
+            score = score + 5;
+            scoreText.setText(""+score);
+            newWord.setText("");
         }
+        else{
+            newWord.setText("");
+        }
+    }
+    public void newGame(View view){
+        newWord.setText("");
+        countDownTimer.cancel();
+        resultview.setText("");
+        btnch.setVisibility(View.VISIBLE);
+        wordDictionary.result.clear();
+        onStart(null);
     }
 }
